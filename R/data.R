ab9b65d415e09cf24f585ac911deb99a3165ccee
Jarred6068 <65281044+Jarred6068@users.noreply.github.com>
2022-04-03 19:22:40 -0700

    package updates
    created examples
    created data
    added documentation

---
+++ R/data.R
@@ -0,0 +1,6 @@
+#' @title 10,000 simulated trios from the MRPC package simulateData()
+#'
+#' @description A list of dataframes where each dataframe represents a single trio from one of the 5 model topologies
+#'
+#' @format a list of length = 10,000 and each element is an n \times 3 dataframe
+"exampleTrios"
---
+++ data-raw/preprocess.R
@@ -0,0 +1,5 @@
+load(file = "C:/Users/Bruin/Documents/GitHub/MRGN_extra/Simulations/Sim_result_Data/mrgn_10000_datasets.RData")
+M1trio=MRPC::simu_data_M1
+exampleTrios=all.datasets
+use_data(exampleTrios)
+use_data(M1trio)
