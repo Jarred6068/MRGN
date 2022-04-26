4ba1ecd4adc3c3636ac3464b5a513a3f065395d2
Jarred6068 <65281044+Jarred6068@users.noreply.github.com>
2022-04-25 17:03:51 -0700

    Update get.conf.R

--- R/get.conf.R
+++ R/get.conf.R
@@ -6,8 +6,8 @@
 #'
 #' @param trios either a list of data frames containing the trios or a single data frame with samples in rows and
 #' trios in the columns
-#' @param PCmat the rotated variables from the whole-genome level PCA
-#' @param alpha the rejection threshold for the pearson correlation test between each trio and the rotated variables
+#' @param PCscores the rotated variables from the whole-genome level PCA
+#' @param FDR the rejection threshold for the pearson correlation test between each trio and the rotated variables
 #' @param save.list (logical) if TRUE the output is saved as a .RData object
 #' @param save.path string specifying the path name of the output
 #' @return a list of dataframes where each element is a dataframe corresponding to a trio and its confounding PCs
@@ -18,7 +18,7 @@
 #'
 
 
-get.conf=function(trios=NULL, PCmat=NULL, alpha=0.05, save.list=FALSE, save.path="/path/to/save/location"){
+get.conf=function(trios=NULL, PCscores=NULL, FDR=0.10, save.list=FALSE, save.path="/path/to/save/location"){
 
   #if data entered as list convert to dataframe
   if(typeof(trios)=="list" & is.null(dim(trios))){
@@ -34,17 +34,18 @@ get.conf=function(trios=NULL, PCmat=NULL, alpha=0.05, save.list=FALSE, save.path
 
   print("Getting list of PCs for each trio...")
   #find PCs that correlate with each trio
-  for (m in 1:(dim(PCmat)[1]-1)) {
-    corr.PCs <- psych::corr.test(PCmat[,m], triomat, use = 'pairwise.complete.obs', adjust = "none", alpha = alpha)
+  for (m in 1:(dim(PCscores)[1]-1)) {
+    corr.PCs <- psych::corr.test(PCscores[,m], triomat, use = 'pairwise.complete.obs', adjust = "none")
     # The p values
     Pvalues <- corr.PCs$p
     Pvalues.nona <- Pvalues[!is.na(Pvalues)]
     All.Pvalues [[m]] <- Pvalues.nona
-    qobj <- qvalue::qvalue(Pvalues.nona, fdr.level = 0.10)
+    qobj <- qvalue::qvalue(Pvalues.nona, fdr.level = FDR)
 
     # Significant associations
     Significant.asso <- qobj$significant
     List.significant.asso1[[m]] <- which(Significant.asso,useNames = TRUE)
+    print(List.significant.asso1)
   }
   print("done!")
   # columns number for trios
@@ -63,14 +64,14 @@ get.conf=function(trios=NULL, PCmat=NULL, alpha=0.05, save.list=FALSE, save.path
     # data
     data <- triomat[,start.col[ii]:end.col[ii]]
     # search in each PCs
-    for (m1 in 1:(dim(PCmat)[1]-1)) {
+    for (m1 in 1:(dim(PCscores)[1]-1)) {
       Match.PCs.col[[m1]] <- intersect(colnames(data),colnames(triomat)[List.significant.asso1[[m1]]])
     }
     List.Match.significant <- which(sapply(Match.PCs.col, function(e) length(e)!=0))
     List.Match.significant.trios[[ii]] <- List.Match.significant
     if(length(List.Match.significant)!=0)
     {
-      data.withPC <- cbind(data,PCmat[,List.Match.significant])
+      data.withPC <- cbind(data,PCscores[,List.Match.significant])
       colnames(data.withPC)[4:ncol(data.withPC)] <- paste("PC",List.Match.significant,sep="")
     }
     else
