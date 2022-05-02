e6bb58fbec9072d4b08967606c12a85981461eb5
Jarred6068 <65281044+Jarred6068@users.noreply.github.com>
2022-05-02 15:32:32 -0700

    updates to get.conf()
    major rework of get.conf to utilize package propagate::bigcor for handling large correlation matrices

--- R/get.conf.R
+++ R/get.conf.R
@@ -8,6 +8,7 @@
 #' frame with samples in rows and trios in the columns
 #' @param PCscores a dataframe of the whole-genome PCA scores with observations in rows and PCs in columns
 #' @param FDR the false discovery rate (default = 0.10)
+#' @param blocksize the number of columns to use in each block of correlation calculations passed to propagate::bigcor
 #' @param save.list (logical) if TRUE the output is saved as a .RData object (default = FALSE)
 #' @param return.list (logical) if TRUE the list of the column indices of significant PCs detected for each trio
 #' is returned (default = TRUE)
@@ -17,12 +18,12 @@
 #' @examples
 #' \dontrun{
 #' #fast example on 40 trios
-#' trio.with.conf=get.conf(trios=WBtrios[1:40], PCscores=WBscores)
+#' trio.with.conf=get.conf(trios=WBtrios[1:40], PCscores=WBscores, blocksize=10)
 #'}
 
 
-get.conf=function(trios=NULL, PCscores=NULL, FDR=0.10, save.list=FALSE,
-                  return.list=TRUE, save.path="/path/to/save/location"){
+get.conf=function(trios=NULL, PCscores=NULL, FDR=0.10, blocksize=2000, save.list=FALSE,
+                       return.list=TRUE, save.path="/path/to/save/location"){
 
   #if data entered as list convert to dataframe
   if(typeof(trios)=="list" & is.null(dim(trios))){
@@ -31,68 +32,51 @@ get.conf=function(trios=NULL, PCscores=NULL, FDR=0.10, save.list=FALSE,
     triomat=trios
   }
 
+  #get indicies of each trio in triomat
+  trio.indices=cbind( start.col = seq(1,dim(triomat)[2],3),
+                      end.col = seq(3,dim(triomat)[2],3))
+  #make colnames unique to avoid error in bigcor
   colnames(triomat) <- make.unique(colnames(triomat)) #duplicated colnames
+  #get the sample sizes used in each pairwise correlation calculation
+  sample.sizes=apply(triomat,2, function(x) length(na.omit(x)))
 
-  All.Pvalues <- list()
-  List.significant.asso1 <- list()
-
-  print("Getting list of signficant PCs for each trio...")
-  #find PCs that correlate with each trio
-  for (m in 1:(dim(PCscores)[1]-1)) {
-    corr.PCs <- psych::corr.test(PCscores[,m], triomat, use = 'pairwise.complete.obs',
-                                 adjust = "none")
-    # The p values
-    Pvalues <- corr.PCs$p
-    Pvalues.nona <- Pvalues[!is.na(Pvalues)]
-    All.Pvalues [[m]] <- Pvalues.nona
-    qobj <- qvalue::qvalue(Pvalues.nona, fdr.level = FDR)
-
-    # Significant associations
-    Significant.asso <- qobj$significant
-    List.significant.asso1[[m]] <- which(Significant.asso,useNames = TRUE)
-
+  #calculate the correlations of each PC with the trios
+  cormat=propagate::bigcor(triomat, PCscores, verbose = T, use="pairwise.complete.obs", size = blocksize)
+  #perform the pearson correlation test, apply qvalue, and return which are significant
+  indmat=apply(cormat[,], 2, q.from.cor, n=sample.sizes, fdr=FDR)
+  #find the PCs that correlated with every column of the trio matrix
+  sig.asso.pcs=apply(indmat[,],1, function(x){list(which(x))})
+  #return the significant PCs for each trio
+  final.list.sig.asso.pcs=apply(trio.indices, 1,
+                                function(x,y){ list(unique(unlist(y[x[1]:x[2]]))) },
+                                y=sig.asso.pcs)
+  #returns the list of significantly associated PCs for each trio
+  if(return.list==TRUE){
+    return(final.list.sig.asso.pcs)
   }
-  print("done!")
-
-  # columns number for trios
-  start.col <- seq(1,dim(triomat)[2],3)
-  end.col <- seq(3,dim(triomat)[2],3)
-
-  number.of.trios <- dim(triomat)[2]/3
-  print(paste0("number of trios = ", number.of.trios))
-
-  Match.PCs.col <- list()
-  List.Match.significant.trios <- list()
-  data.sets=list()
-
-  print("Extracting significant PCs...")
-  for (ii in 1:number.of.trios) {
-    # data
-    data <- triomat[,start.col[ii]:end.col[ii]]
-    # search in each PCs
-    for (m1 in 1:(dim(PCscores)[1]-1)) {
-      Match.PCs.col[[m1]] <- intersect(colnames(data),colnames(triomat)[List.significant.asso1[[m1]]])
-    }
-    List.Match.significant <- which(sapply(Match.PCs.col, function(e) length(e)!=0))
-    List.Match.significant.trios[[ii]] <- List.Match.significant
-    # if(length(List.Match.significant)!=0){
-    #
-    #   data.withPC <- cbind(data,PCscores[,List.Match.significant])
-    #   colnames(data.withPC)[4:ncol(data.withPC)] <- paste("PC",List.Match.significant,sep="")
-    #
-    # }else{
-    #   data.withPC <- data
-    # }
-    #   data.sets[[ii]]=data.withPC
-  }
-  print("done!")
-
   if(save.list==TRUE){
-    print("saving...")
-    save(List.Match.significant.trios, file = paste0(save.path,".RData"))
-    print("done!")
+    save(final.list.sig.asso.pcs, file = paste0(save.path,".RData"))
   }
 
-    return(List.Match.significant.trios)
 
 }
+
+#' A function to calculate the pearson correlation p-values and apply the qvalue correction
+#'
+#' @param r either (1) a single pearson correlation coefficient or (2) a vector of correlation coefficients
+#' @param n the sample size (if length(r)>1 then n must be a vector if sample sizes vary)
+#' @param fdr the false discovery rate
+#' @return a logical vector indicating significant correlations after qvalue correction
+
+q.from.cor=function(r, n, fdr){
+
+  #calculate t values from vector of correlations
+  t=r*sqrt((n-2)/(1-r^2))
+  #obtain the pvalue from two-tailed test
+  p=2 * (1 -pt(q = abs(t), df = n-2))
+  #apply qvalue correction and return significant entries
+  sig=qvalue::qvalue(p, fdr.level = fdr)$significant
+  return(sig)
+
+}
+
