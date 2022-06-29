65e47f3bf074a99e57f7efad61c01f2f2c325b1e
Jarred6068 <65281044+Jarred6068@users.noreply.github.com>
2022-06-29 13:05:23 -0700

    Update simulate_trios_with_conf.R
    added in 4 functions for generating data from a graph topology

--- R/simulate_trios_with_conf.R
+++ R/simulate_trios_with_conf.R
@@ -219,8 +219,414 @@ simData=function (theta, model, b0.1, b.snp, b.med, sd.1, G=NULL, u=NULL){
   }, stop("Model not included or missing"))
 }
 
+#################################################
+
+#' A function to simulate a graph with confounder, intermediate, and common child variables
+#'
+#' This function simulates a graph adjacency matrix for a desired topology such as M0 - M4, custom, or random topological
+#' graphs such that they include confounder, intermediate, and common child variables.
+#'
+#' @param model a string specifying one of "model0","model1", "model2", "model3","model4", or "custom"
+#' @param conf.num.vec a numeric vector of length 4 containing the number of confounder, known confounder, intermediate,
+#' and common child variables (in that order). To exclude a variable type input a zero at the given position.
+#' @param number.of.T a numeric indicating the number of T variables desired for model == "custom" only
+#' @param number.of.V a numeric indicating the number of V variables desired for model == "custom" only
+#' @param struct For use when when model == "custom". Either (1) a sub-adjacency matrix of dimension
+#' (number.of.V + number.of.T  X  number.of.V + number.of.T) definining the topology of the V and T nodes
+#' or (2) the string "random" denoting a random topology
+#' @return an adjacency matrix
+#' @export gen.graph.skel
+#' @examples
+#' # generate a model 0 graph with one of each type of confounding variables
+#' adj = gen.graph.skel(model = "model0", conf.num.vec = c(1,1,1,1))
+
+#creates the graph skeleton for each model: contains u, k, w, and z variables in adj.
+gen.graph.skel = function(model, conf.num.vec, number.of.T, number.of.V, struct){
+
+  #preallocate names
+  conf.node.names=NULL
+  letter.id = c("U","K","W","Z")
+  #create all confounder names
+  if(sum(conf.num.vec)>0){
+    for(i in 1:length(conf.num.vec)){
+      conf.node.names = append(conf.node.names, paste0(letter.id[i], c(1:conf.num.vec[i])))
+    }
+  }
+  #preallocate adjecency matrix:
+  if(model == "custom"){
+    #catch missing inputs for number of T and V nodes for custom graphs
+    if(missing(number.of.T) | missing(number.of.V)){
+      stop("Missing one of \"number.of.T\" or \"number.of.V\" for model = \"custom\" ")
+    }
+    #catch missing struct input for custom graph
+    if(missing(struct)){
+      stop("Missing arg: \"struct\" must enter a desired structure for V and T nodes when model = \"custom\" " )
+    }
+    #for custom graphs of chosen size
+    A = matrix(0, nrow = sum(conf.num.vec)+number.of.T+number.of.V,
+               ncol = sum(conf.num.vec)+number.of.T+number.of.V)
+
+    row.names(A) = colnames(A) = c(paste0("V", c(1:number.of.V)),
+                                   paste0("T", c(1:number.of.T)),
+                                   conf.node.names)
+  }else{
+    #for the other M0-M4 models
+    A = matrix(0, nrow = sum(conf.num.vec)+3, ncol = sum(conf.num.vec)+3)
+    row.names(A) = colnames(A) = c("V1","T1","T2", conf.node.names)
+  }
+
+  #----------model-0-----------
+  switch(model, model0 = {
+    A[1,2] = 1
+    #confounders
+    for(i in 1:2){
+      for(j in 2:3){
+        A[which(grepl(letter.id[i],conf.node.names))+3, j] = 1
+      }
+    }
+    #intermediate
+    A[which(grepl("W",conf.node.names))+3, 3] = 1
+    A[2, which(grepl("W",conf.node.names))+3] = 1
+
+    #common child
+    for(k in 2:3){
+      A[k, which(grepl("Z",conf.node.names))+3] = 1
+    }
+
+    return(A)
+    #----------model-1-----------
+  }, model1 = {
+
+    A[1,2] = 1
+    A[2,3] = 1
+    #confounders
+    for(i in 1:2){
+      for(j in 2:3){
+        A[which(grepl(letter.id[i],conf.node.names))+3, j] = 1
+      }
+    }
+    #intermediate
+    A[which(grepl("W",conf.node.names))+3, 3] = 1
+    A[2, which(grepl("W",conf.node.names))+3] = 1
+
+    #common child
+    for(k in 2:3){
+      A[k, which(grepl("Z",conf.node.names))+3] = 1
+    }
+
+    return(A)
+    #----------model-2-----------
+  }, model2 = {
+
+    A[1,2] = 1
+    A[3,2] = 1
+    #confounders
+    for(i in 1:2){
+      for(j in 2:3){
+        A[which(grepl(letter.id[i],conf.node.names))+3, j] = 1
+      }
+    }
+    #intermediate
+    A[which(grepl("W",conf.node.names))+3, 2] = 1
+    A[3, which(grepl("W",conf.node.names))+3] = 1
+
+    #common child
+    for(k in 2:3){
+      A[k, which(grepl("Z",conf.node.names))+3] = 1
+    }
+
+    return(A)
+    #----------model-3-----------
+  }, model3 = {
+
+    A[1,2] = 1
+    A[1,3] = 1
+    #confounders
+    for(i in 1:2){
+      for(j in 2:3){
+        A[which(grepl(letter.id[i],conf.node.names))+3, j] = 1
+      }
+    }
+    #intermediate
+    A[which(grepl("W",conf.node.names))+3, 3] = 1
+    A[2, which(grepl("W",conf.node.names))+3] = 1
+
+    #common child
+    for(k in 2:3){
+      A[k, which(grepl("Z",conf.node.names))+3] = 1
+    }
+
+    return(A)
+    #----------model-4-----------
+  }, model4 = {
+
+    A[1,2:3] = 1
+    A[2,3] = 1
+    A[3,2] = 1
+    #confounders
+    for(i in 1:2){
+      for(j in 2:3){
+        A[which(grepl(letter.id[i],conf.node.names))+3, j] = 1
+      }
+    }
+    #intermediate
+    A[which(grepl("W",conf.node.names))+3, 3] = 1
+    A[2, which(grepl("W",conf.node.names))+3] = 1
+
+    #common child
+    for(k in 2:3){
+      A[k, which(grepl("Z",conf.node.names))+3] = 1
+    }
+
+    return(A)
+    #----------custom-model-----------
+  }, custom = {
+
+    if(struct == "random"){
+      #random topology for V and T
+      A.sub = as.matrix(igraph::get.adjacency(igraph::random.graph.game(number.of.V+number.of.T, p = 0.5, directed = T)))
+      #remove T --> V
+      A.sub[,1:number.of.V] = 0
+      #remove V --> V
+      A.sub[1:number.of.V, 1:number.of.V] = 0
+
+      #insert topology
+      A[1:(number.of.V+number.of.T), 1:(number.of.V+number.of.T)] = A.sub
+
+      #custom structure connectivity for V and T
+    }else{
+      A[1:(number.of.V+number.of.T), 1:(number.of.V+number.of.T)] = struct
+    }
+
+    #confounders
+    for(i in 1:2){
+      for(j in ((1:number.of.T)+number.of.V)){
+        A[which(grepl(letter.id[i],conf.node.names))+number.of.T+number.of.V, j] = 1
+      }
+    }
+    #intermediate
+    A[which(grepl("W",conf.node.names))+number.of.T+number.of.V,
+      sample(c(1:number.of.T)+number.of.V, round(runif(1,1, number.of.T)), replace = F)] = 1
+    A[sample(c(1:number.of.T)+number.of.V, round(runif(1,1, number.of.T)), replace = F),
+      which(grepl("W",conf.node.names))+number.of.T+number.of.V] = 1
+
+    #common child
+    for(k in ((1:number.of.T)+number.of.V)){
+      A[k, which(grepl("Z",conf.node.names))+number.of.T+number.of.V] = 1
+    }
+
+    diag(A) = 0
+    return(A)
+
+  }, stop("Model not included or missing"))
+
+}
 
 
+#################################################
+
+#' A function to find the parents of a given node in the graph
+#'
+#' This function uses the adjacency matrix of a graph to identify the parents of a desired node in the graph
+#'
+#' @param location the column index of the node of interest in the adjacency matrix
+#' @param Adjacency an adjacency matrix for the graph
+#' @return an adjacency matrix
+
+find.parents = function(Adjacency, location){
+  #define letter identifier for node types
+  letter.id = c("V","T","U","K","W")
+  all.parents = row.names(Adjacency)[which(Adjacency[, location] == 1)]
+  #allocate each type of parent indexes to list by type
+  parent.list = lapply(letter.id,function(x,y,z){if(any(grepl(x,y))) match(y[which(grepl(x,y))], z) else NA},
+                       y = all.parents, z = colnames(Adjacency))
+  names(parent.list)=c("V", "T","U","K","W")
+
+  return(parent.list)
+}
+
+
+#################################################
+
+#' A function to generate effects for effects in the graph
+#'
+#' @param parental.list a list of the parents of the given node
+#' @param b.snp the desired effect for all edges with the genetic variant
+#' @param b.med the desired effect for the mediation edge
+#' @param num.z the number of child variables
+#' @param coef.range.list a list of length 4 containing the mininum and maximum effect size for U,K,W, and Z variables
+#' (in that order)
+#' @param neg.freq the frequency of negative effects for a given variable
+#' @return a list of length 6 containing the simulated effects for each type of variable
+
+gen.conf.coefs=function(parental.list = NULL, b.snp=NULL, b.med=NULL, num.z=NULL, coef.range.list=NULL, neg.freq=0.5){
+
+  #simulation of effects of variables
+  #preallocate list of effects
+  sim.effects = vector("list", length = 6)
+  #name elements according to var type
+  names(sim.effects) = c("V","T","U","K","W", "Z")
+  #pass the desired snp and mediation effects
+  sim.effects[[1]] = b.snp
+  sim.effects[[2]] = b.med
+  #simulate effects of U,K,W, and Z vars from ranges in coef.range.list
+  for(i in 3:6){
+    if(i == 6){
+      number.of.confs = num.z
+    }else{
+      number.of.confs = length(parental.list[[i]])
+    }
+    ct1 = rbinom(number.of.confs, 1, neg.freq)
+    w1 = runif(number.of.confs, min = coef.range.list[[i-2]][1], max = coef.range.list[[i-2]][2])
+    w1[which(ct1==1)]=-1*w1[which(ct1==1)]
+    sim.effects[[i]]=w1
+  }
+  #}
+
+  return(sim.effects)
+
+}
+
+
+#################################################
+
+#' A function to simulate data from a desired graph topology
+#'
+#' This function wraps gen.graph.skel(), gen.conf.coef(), and find.parents() to simulate data from a given graph topology
+#' using the topological order.
+#'
+#' @param model a string specifying one of "model0","model1", "model2", "model3","model4", or "custom"
+#' @param theta the frequency of the minor allele of the genetic variant
+#' @param b0.1 the intercept
+#' @param b.snp the effect of the genetic variant(s)
+#' @param b.med the effect between the molecular phenotypes
+#' @param sd.1 the residual standard deviation (noise)
+#' @param conf.num.vec a numeric vector of length 4 containing the number of confounder, known confounder, intermediate,
+#' and common child variables (in that order). To exclude a variable type, input a zero at the given position.
+#' @param number.of.T a numeric indicating the number of T variables desired for model == "custom" only
+#' @param number.of.V a numeric indicating the number of V variables desired for model == "custom" only
+#' @param struct For use when when model == "custom". Either (1) a sub-adjacency matrix of dimension
+#' (number.of.V + number.of.T  X  number.of.V + number.of.T) definining the topology of the V and T nodes
+#' or (2) the string "random" denoting a random topology
+#' @param simulate.confs (logical) indicating if U and K nodes should be simulated (default = TRUE).
+#' @param conf.mat For use when simulate.confs = FALSE. A dataframe with confounders (U and K nodes) in columns
+#' and observations in rows. Note that the number of observations fixes the sample size for the simulation.
+#' @param sample.size a numeric value greater than 1 specifying the sample size when simulate.confs = TRUE
+#' @param plot.graph (logical) when FALSE, graph is not plotted (default is TRUE)
+#' @param conf.coef.ranges a list of length 4 representing the U,K,W, and Z (in that order) node effects where each list
+#' element is a vector of length 2 giving the minimum and maximum effect sizes for the given node. default values follow
+#' from the simulations of Yang et. al., 2017
+#' @return an adjacency matrix
+#' @export simData.from.graph
+#' @examples
+#' # simulate 1000 samples from a model 1 graph with one of each confounder and
+#' # plot the graph.
+#' X=simData.from.graph(theta=0.2,
+#'                      model="model1",
+#'                      b0.1=0,
+#'                      b.snp=0.8,
+#'                      b.med=0.6,
+#'                      sd.1=0.8,
+#'                      conf.num.vec = c(1,1,1,1),
+#'                      simulate.confs = TRUE,
+#'                      plot.graph = TRUE,
+#'                      sample.size = 1000,
+#'                      conf.coef.ranges=list(U=c(0.15,0.5), K=c(0.01, 0.1),
+#'                                            W=c(0.15,0.5), Z=c(1, 1.5)))
+
+simData.from.graph = function(model, theta, b0.1, b.snp, b.med, sd.1, conf.num.vec, number.of.T, number.of.V,
+                              struct, simulate.confs = TRUE, conf.mat, sample.size, plot.graph = TRUE,
+                              conf.coef.ranges=list(U=c(0.15,0.5), K=c(0.01, 0.1), W=c(0.15,0.5), Z=c(1, 1.5))){
+
+  #for use of real confounders or setting sample size to simulate all confounders
+  if(simulate.confs==TRUE){
+    if(missing(sample.size)){stop("missing sample.size: must supply one of G or sample.size")}
+    if(sum(conf.num.vec[1:2])==0){stop("confounders = 0: Must specify the number of U and K nodes")}
+    N = sample.size
+    conf.mat = mvtnorm::rmvnorm(N, mean = rnorm(sum(conf.num.vec[1:2])),
+                                sigma = diag(sum(conf.num.vec[1:2])))
+    if(conf.num.vec[1]==0){
+      colnames(conf.mat) = c(paste0("K", 1:conf.num.vec[2]))
+    }else if(conf.num.vec[2]==0){
+      colnames(conf.mat) = c(paste0("U", 1:conf.num.vec[1]))
+    }else{
+      colnames(conf.mat) = c(paste0("U", 1:conf.num.vec[1]), paste0("K", 1:conf.num.vec[2]))
+    }
+  }else{
+    N = dim(conf.mat)[1]
+  }
+
+  #generate the graph
+  graph.adj = gen.graph.skel(model = model, conf.num.vec = conf.num.vec, number.of.T = number.of.T,
+                             number.of.V = number.of.V, struct = struct)
+  #preallocate data matrix
+  X = as.data.frame(matrix(0, nrow = N, ncol = dim(graph.adj)[2]))
+  colnames(X) = colnames(graph.adj)
+  #add in the confounding variables
+  X[,match(colnames(conf.mat), colnames(graph.adj))] = conf.mat
+  #convert adjacency to igraph
+  graph.obj = igraph::graph_from_adjacency_matrix(graph.adj)
+  if(plot.graph == TRUE){
+    igraph::plot.igraph(graph.obj, layout = layout_nicely, edge.arrow.size = 0.2)
+  }
+  #get the topological ordering
+  topo.order = colnames(graph.adj)[as.vector(igraph::topo_sort(graph.obj))]
+
+  for(i in 1:length(topo.order)){
+
+    #generate V nodes in topo order
+    location = match(topo.order[i], colnames(graph.adj))
+
+    if(grepl("V", topo.order[i])){
+      X[,location] = c(sample(c(0, 1, 2), size = N, replace = TRUE,
+                              prob = c((1 -theta)^2, 2 * theta * (1 - theta), theta^2)))
+
+      #generate T nodes in topo order
+    }else if(grepl("T", topo.order[i])){
+
+      parent.list = find.parents(Adjacency = graph.adj, location = location)
+
+      #catch nodes with no parents of any kind
+      if(sum(unlist(lapply(parent.list, is.na)))==5){
+        #generate node
+        X[, location] = rnorm(n = N, mean = b0.1, sd = sd.1)
+      }else{
+        #get parent idx
+        parent.idx = which(unlist(lapply(parent.list, function(x) !is.na(x[1]))))
+        #generate confounder effects
+        coefs.list = gen.conf.coefs(parental.list = parent.list, b.snp = b.snp, b.med = b.med,
+                                    num.z = conf.num.vec[4], coef.range.list=conf.coef.ranges)
+        print(coefs.list[3:4])
+        #find which effects to use accord to parental list
+        which.coefs = match(names(parent.idx), names(coefs.list))
+        #generate node
+        X[, location] = rnorm(n = N, mean = b0.1 +
+                                as.matrix(X[, unlist(parent.list[parent.idx])])%*%unlist(coefs.list[which.coefs]),
+                              sd = sd.1)
+
+      }
+
+      #create intermediate variables
+    }else if(grepl("W", topo.order[i])){
+
+      parent.list = find.parents(Adjacency = graph.adj, location = location)
+      coefs.list = gen.conf.coefs(parental.list = parent.list, b.snp = b.snp, b.med = b.med,
+                                  num.z = length(parent.list$T), coef.range.list=conf.coef.ranges)
+      X[,location] = rnorm(n = N, mean = b0.1 + as.matrix(X[,parent.list$T])%*%coefs.list$W, sd = sd.1)
+
+      #create child variables
+    }else if(grepl("Z", topo.order[i])){
+      parent.list = find.parents(Adjacency = graph.adj, location = location)
+      coefs.list = gen.conf.coefs(parental.list = parent.list, b.snp = b.snp, b.med = b.med,
+                                  num.z = length(parent.list$T), coef.range.list=conf.coef.ranges)
+      X[,location] = rnorm(n = N, mean = b0.1 + as.matrix(X[,parent.list$T])%*%coefs.list$Z, sd = sd.1)
+    }
+
+  }
+
+  return(X)
+
+}
 
 
 
