bf5dfe95ebc535f8d70b6c0525642177f0a79331
Jarred6068 <65281044+Jarred6068@users.noreply.github.com>
2022-07-05 15:14:07 -0700

    Update class.vec.R
    fixed a bug that caused M4 category to be skipped over and thus not included as a possible model class

--- R/class.vec.R
+++ R/class.vec.R
@@ -26,31 +26,28 @@ class.vec=function(vec=NULL){
   which.mod=row.names(ind.mat)[prodlim::row.match(vec[1:4], ind.mat)]
   #print(which.mod)
   #if no match is found allocate to other
-  if(is.na(row.match(vec[1:4], ind.mat))){mt="Other"}
+  if(is.na(row.match(vec[1:4], ind.mat))){
 
-  else if(which.mod=="M2/M4"){
+    return(mt="Other")
+
+  }else if(which.mod=="M2/M4"){
 
     #handle M2 and M4 by differentiating based on
     #marginal tests
     zp=vec[5:6]
     #if both marginals are sig we
     #allocate to M4
-    if(sum(zp)==2){mt="M4"}
+    if(sum(zp)==2){return(mt="M4")}
     #if both marginals are insig we allocate to other
-    if(sum(zp)==0){mt="Other"}
+    if(sum(zp)==0){return(mt="Other")}
 
     #if only one of the marginals is sig we determine
     #which direction and allocate to M2.1 or M2.2
-    else if(sum(zp==c(0,1))==2){mt="M2.1"}
-
-    else if(sum(zp==c(1,0))==2){mt="M2.2"}
-
-    #all other conditions allocated to class other
-    else{mt="Other"}
+    if(sum(zp==c(0,1))==2){return(mt="M2.1")}
+    if(sum(zp==c(1,0))==2){return(mt="M2.2")}
 
     #if not M2/M4 and matched to gt, return model class
-  }else{mt=which.mod}
-
-  return(mt)
-
+  }else{
+    return(mt=which.mod)
+  }
 }
