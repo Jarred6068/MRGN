5b7f6bc8999106881c92ab42f50c65462f94550e
Jarred6068 <65281044+Jarred6068@users.noreply.github.com>
2022-07-11 14:21:36 -0700

    Update preprocess.R
    changed the number of trios in the example data to 200 for faster loading

--- data-raw/preprocess.R
+++ data-raw/preprocess.R
@@ -5,6 +5,6 @@ WBtrios=wbtriolist
 WBtrios=lapply(WBtrios, function(x){row.names(x)=c(1:nrow(x));return(x)})
 WBscores=PCs.matrix
 row.names(WBscores)=c(1:nrow(WBscores))
-usethis::use_data(WBtrios, overwrite = T)
+usethis::use_data(WBtrios[1:200], overwrite = T)
 usethis::use_data(WBscores, overwrite = T)
 usethis::use_data(M1trio, overwrite = T)
