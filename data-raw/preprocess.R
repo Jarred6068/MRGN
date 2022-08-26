735a28d525367d795c17e427094ca81c602e0d27
Jarred6068 <65281044+Jarred6068@users.noreply.github.com>
2022-08-25 18:04:56 -0700

    Update preprocess.R

--- data-raw/preprocess.R
+++ data-raw/preprocess.R
@@ -1,10 +1,12 @@
 load("C:/Users/Bruin/Documents/GitHub/MRGN_extra/exampleWBtrios.RData")
 load("C:/Users/Bruin/Documents/GitHub/MRGN_extra/WBpcs.RData")
+CNAtrios=readRDS("C:/Users/Bruin/Documents/GitHub/MRGN_extra/100triosdata.Rdata")
 M1trio=MRPC::simu_data_M1
-WBtrios=wbtriolist[1:200]
+WBtrios=wbtriolist[1:100]
 WBtrios=lapply(WBtrios, function(x){row.names(x)=c(1:nrow(x));return(x)})
 WBscores=PCs.matrix
 row.names(WBscores)=c(1:nrow(WBscores))
 usethis::use_data(WBtrios, overwrite = T)
 usethis::use_data(WBscores, overwrite = T)
 usethis::use_data(M1trio, overwrite = T)
+usethis::use_data(CNAtrios, overwrite = T)
