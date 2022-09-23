36719d8179b4c05545f4a1db25744eb3ca976c74
Jarred6068 <65281044+Jarred6068@users.noreply.github.com>
2022-09-23 16:07:25 -0700

    Update preprocess.R

--- data-raw/preprocess.R
+++ data-raw/preprocess.R
@@ -5,8 +5,14 @@ M1trio=MRPC::simu_data_M1
 WBtrios=wbtriolist[1:100]
 WBtrios=lapply(WBtrios, function(x){row.names(x)=c(1:nrow(x));return(x)})
 WBscores=PCs.matrix
+triomat = do.call('cbind', WBtrios)
+snp.idx = seq(1, dim(triomat)[2]-2, 3)
+WBsnps = triomat[,snp.idx]
+WBgenes = triomat[,-snp.idx]
 row.names(WBscores)=c(1:nrow(WBscores))
 usethis::use_data(WBtrios, overwrite = T)
 usethis::use_data(WBscores, overwrite = T)
 usethis::use_data(M1trio, overwrite = T)
 usethis::use_data(CNAtrios, overwrite = T)
+usethis::use_data(WBgenes, overwrite = T)
+usethis::use_data(WBsnps, overwrite = T)
